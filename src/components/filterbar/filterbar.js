9975f619416a86be8b5ec49e948e8cefee6b5d50
Jorie MacDonald <59182265+joriemacd@users.noreply.github.com>
2023-08-14 11:21:23 -0400

    style filterbar

--- src/components/filterbar/filterbar.js
+++ src/components/filterbar/filterbar.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import './filterbar.css'
 
 const FilterBar = ({ selectedCategory, onCategoryChange }) => {
   const categories = ["All", "Category A", "Category B"]; // Add more categories as needed
 
+  const [activeCategory, setActiveCategory] = useState(selectedCategory);
+
+  const handleCategoryChange = (category) => {
+    setActiveCategory(category);
+    onCategoryChange(category);
+  };
+
   return (
     <div>
-      <label>Filter by Category:</label>
+      {/* <label>Filter by Category:</label> */}
       {categories.map(category => (
-        <label key={category}>
-          <input
+        // <label key={category}>
+        <label
+          key={category}
+          className={activeCategory === category ? "active" : ""}
+        >
+         <input
             type="radio"
             name="category"
             value={category}
-            checked={selectedCategory === category}
-            onChange={() => onCategoryChange(category)}
+            checked={activeCategory === category}
+            onChange={() => handleCategoryChange(category)}
           />
           {category}
         </label>
--- src/components/subpage/subpage.js
+++ src/components/subpage/subpage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import turtleImage from "../images/turtle.jpeg"
+import './subpage.css'
 
 const SubPage = ({ id, title, coverImage, category }) => (
   <div className="sub-page">
+    <div className="cover-image">
     <img src={coverImage} alt={`Cover for ${title}`} />
-    <Link to={`/work/${title}`}>{title}</Link>
+    </div>
+    <Link className="title" to={`/work/${title}`}>{title}</Link>
   </div>
 );
 
