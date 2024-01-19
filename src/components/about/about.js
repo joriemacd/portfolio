f7c861eaa319049efe52d7c40b982fa682abe502
Jorie MacDonald <59182265+joriemacd@users.noreply.github.com>
2024-01-19 01:03:45 -0800

    updated about to make it functional for now

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import jorieImage from '../images/jorie-black-background.jpg'
+import '../about/about.css'
 
 const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <p>This page is currently under construction! </p>
+      <div className='content-container'>
+        <img className="jorieImage" src={jorieImage} alt="Description" />
+        <p className='about-text'>I'm Jorie, a '25 at Dartmouth College studying Computer Science & Human Centered Design.
+        <p className='about-text'>I'm from San Diego, California and recently studied abroad on Semester at Sea. On campus, I am a tour guide and an executive board member of my sorority, Kappa Kappa Gamma.  </p>
+
+          
+           </p>
+      </div>
     </div>
   );
 };
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import jorieImage from '../images/jorie-black-background.jpg'
 import '../home/home.css'
 
 const Home = () => {
   return (
     <div>
       <h1>Jorie MacDonald</h1>
-        <img className="jorieImage" src={jorieImage} alt="Description" />
         <p className='greeting-container'>Hi! I'm a student at Dartmouth College studying Computer Science and Human-Centered Design.</p>
       </div>
   );
